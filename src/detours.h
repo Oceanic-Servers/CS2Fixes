689638169b8526458d0a2523d15f2e0eda605e89
zonical <zonicalguy@gmail.com>
2024-05-26 11:49:32 +1200

    Revert "Remove TakeDamageOld detour, add CMessage and reimplement ShowMessage for env_message"
    This reverts commit 6224e2e5f28d921e7650bdfd8e70aa66261f22e2.

--- src/detours.h
+++ src/detours.h
@@ -55,6 +55,7 @@ void FASTCALL Detour_UTIL_SayText2Filter(IRecipientFilter &, CCSPlayerController
 bool FASTCALL Detour_IsHearingClient(void*, int);
 void FASTCALL Detour_TriggerPush_Touch(CTriggerPush* pPush, CBaseEntity* pOther);
 void FASTCALL Detour_CGameRules_Constructor(CGameRules *pThis);
+void FASTCALL Detour_CBaseEntity_TakeDamageOld(CBaseEntity *pThis, CTakeDamageInfo *inputInfo);
 bool FASTCALL Detour_CCSPlayer_WeaponServices_CanUse(CCSPlayer_WeaponServices *, CBasePlayerWeapon *);
 bool FASTCALL Detour_CEntityIdentity_AcceptInput(CEntityIdentity* pThis, CUtlSymbolLarge* pInputName, CEntityInstance* pActivator, CEntityInstance* pCaller, variant_t* value, int nOutputID);
 void* FASTCALL Detour_CNavMesh_GetNearestNavArea(int64_t unk1, float* unk2, unsigned int* unk3, unsigned int unk4, int64_t unk5, int64_t unk6, float unk7, int64_t unk8);
